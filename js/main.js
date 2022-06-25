3b76ed3b4f8713048c6c474080024cfc78a77912
hoangnm-ndm <hoangnm.ndm@gmail.com>
2022-06-26 00:03:31 +0700

    update

--- js/main.js
+++ js/main.js
@@ -17,3 +17,20 @@ $(window).on("load", function(){
   slideShow();
 
 })
+
+$(document).ready(function(){
+  peopleFilter($(".filter-btn.active").attr("data-target"))
+  $(".filter-btn").click(function(){
+    if(!$(this).hasClass("active")) {
+      peopleFilter($(this).attr("data-target"))
+    }
+  })
+  function peopleFilter(target){
+    $(".filter-btn").removeClass("active");
+    $(".filter-btn[data-target='"+target+"']").addClass("active");
+    $(".people-item").hide();
+    $(".people-item[data-category='"+target+"']").fadeIn()
+  }
+})
+
+
